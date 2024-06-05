0eea7f96ad5457ed11f3cc55b35fc29996ea69e4
onenoz <wulannuranam@gmail.com>
2024-06-05 22:22:44 +0700

    page perusahaan

---
+++ MerakiWebsite-VisualStudio/MerakiWebsite-VisualStudio/Pages/Perusahaan.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MerakiWebsite_VisualStudio.Pages
+{
+    public class PerusahaanModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
