c6735fdfa5db6adfbf6146936c41fb477358d67f
onenoz <wulannuranam@gmail.com>
2024-06-05 22:37:38 +0700

    page kontak

---
+++ MerakiWebsite-VisualStudio/MerakiWebsite-VisualStudio/Pages/Kontak.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MerakiWebsite_VisualStudio.Pages
+{
+    public class KontakModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
