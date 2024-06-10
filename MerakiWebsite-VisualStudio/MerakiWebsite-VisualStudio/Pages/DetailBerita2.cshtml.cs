a5aac9d936673db250e9c6e28603a393fe32359f
Pierediaz <pierediaz.putra@gmail.com>
2024-06-11 01:12:15 +0700

    halaman berita + halaman detail berita 1,2,3

---
+++ MerakiWebsite-VisualStudio/MerakiWebsite-VisualStudio/Pages/DetailBerita2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MerakiWebsite_VisualStudio.Pages
+{
+    public class DetailBerita2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
